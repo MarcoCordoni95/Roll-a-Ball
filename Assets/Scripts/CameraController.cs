f932c97c94d02bade11b7b038a7b8106cc1b460c
Marco Cordoni <marco.cordoni@studenti.unimi.it>
2018-10-21 18:09:30 +0200

    Roll a Ball

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player; // mantiene riferimento al giocatore
+
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+        offset = transform.position - player.transform.position; // calcolo la differenza per mantenere la camera fissa 
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () { // usiamo LateUpdate per essere sicuri 
+        transform.position = player.transform.position + offset;
+	}
+}
